b9efb124d0fc38dda3ff97735d0ddc8ff755f067
DPfeyfer <Skydenfer737@gmail.com>
2023-01-26 14:44:39 +0300

    Study Lesson 1 complete

--- src/For_study.java
+++ src/For_study.java
@@ -1,5 +1,18 @@
 public class For_study {
-    public static void main(String[] args) {
 
+    public static void printSeparator() {
+        System.out.println("++++++++++++++++++++++");
+        System.out.println("----------------------");
+    }
+    public static void main(String[] args) {
+        int[] issueByMonths = {4, 6, 7, 9, 2, 5, 12, 3, 7, 10, 6, 7, 1, 8};
+        printSeparator();
+        for (int i = 0; i < issueByMonths.length; i++) {
+            System.out.println(issueByMonths[i]);
+            if ((i + 1) % 3 == 0) {
+                printSeparator();
+            }
+        }
+        printSeparator();
     }
 }
